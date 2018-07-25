c7ff05d4cacbdebaa84131616365447216de0af7
github username <github email address>
2018-07-25 09:43:33 -0500

    mock test

--- author.rb
+++ author.rb
@@ -1,1 +1,32 @@
+class Author
 
+  attr_reader :books, :title, :author
+
+    @@all = []
+
+  def initialize(books)
+    @books = books
+    @title = title
+    @author = author
+  end
+
+  def books(books, author)
+    books.each do |books, author|
+    @@all  << author
+  end
+
+  #
+  # def write_book
+  #
+  # end
+  #
+  # def total_words
+  #
+  # end
+  #
+  # def most_words
+  #
+  # end
+
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,35 @@
+class Book
 
+  attr_reader :author, :title, :book
+
+  @@all = []
+
+  def initialize(author, title, book)
+    @author = author
+    @title = title
+    @book = book
+  end
+
+  def self.book(book)
+    book.each do |books|
+    @@all  << books
+    binding.pry
+  end
+
+  def author(book)
+    book.find |author|
+    binding.pry
+  end
+
+
+  def title(book, title)
+    book.find |title|
+  end
+
+  # def word_count
+  #
+  # end
+
+  end
+
+end
